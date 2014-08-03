dac71d476c7977be5d50434f841d1d97b1e73062
mengyangservice <serv.my@qq.com>
2014-08-03 16:43:51 +0800

    Change to force overwrite
    for redeploy: Change to force overwrite

--- patch.sh
+++ patch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp lib/HttpPostUtils.class /opt/apache-tomcat-7.0.54/webapps/myservice/WEB-INF/classes/com/ifp/weixin/util/
+cp -f lib/HttpPostUtils.class /opt/apache-tomcat-7.0.54/webapps/myservice/WEB-INF/classes/com/ifp/weixin/util/
